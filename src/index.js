2797a16bd447858ec5babf79a006dd47aa59cf85
William Bae <wbae912@gmail.com>
2019-08-28 12:31:07 -0700

    completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-'use strict';
-/* global shoppingList, store, api */
+import './index.css';
+import $ from 'jQuery';
+import 'normalize.css';
+import api from './api';
+import shoppingList from './shopping-list';
+import store from './store';
+
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
